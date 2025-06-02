6c5f29b32f96333587476aaed747d0f733d0ee94
constavro <constavro@gmail.com>
2025-06-02 13:59:44 +0300

    Fixed bug in deleting trip

--- middleware/tripMiddleware.js
+++ middleware/tripMiddleware.js
@@ -2,11 +2,12 @@ const Trip = require('../models/Trip');
 
 // Middleware: Validate Trip Ownership
 const validateOwnership = async (req, res, next) => {
+
     try {
       const trip = await Trip.findById(req.params.id);
       if (!trip) return sendError(res, 404, 'Trip not found');
   
-      if (trip.host.toString() !== req.user.id)
+      if (trip.organizer.toString() !== req.user.id)
         return sendError(res, 403, 'Unauthorized to modify this trip');
   
       req.trip = trip; // Attach trip to request for further use
